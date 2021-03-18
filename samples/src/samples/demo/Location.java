5ca2f7753b9809b37084c4c9b6eeea5540cadd28
chungseng99 <seng990417@gmail.com>
2021-03-18 10:29:14 +0800

    third commit

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,21 @@
+package samples.demo;
+
+public class Location {
+	
+	//attribute declaration
+	private String location;
+	
+	//setter
+	public void setLocation(String location) {
+		
+		this.location= location;
+	}
+	
+	//getter
+	public String getLocation() {
+		
+		return location;
+		
+	}
+	
+}
